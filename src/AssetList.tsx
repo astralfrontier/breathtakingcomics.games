14ac1b0af1452c3e7c54a3849293a393de66811a
Bill Garrett <garrett@astralfrontier.org>
2025-11-26 10:34:58 -0800

    Show assets in groups

--- src/AssetList.tsx
+++ src/AssetList.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router";
 import type { BreathtakingAssetPackage } from "./AssetTypes";
+import { groupBy } from "ramda";
+import { useMemo } from "react";
 
 interface AssetListProps {
   prefix: string;
@@ -10,16 +12,31 @@ interface AssetListProps {
 function AssetList(props: AssetListProps) {
   const { prefix, assetPackage, children } = props;
 
+  const groupedAssets = useMemo(
+    () => groupBy((asset) => asset.group ?? "", assetPackage.assets),
+    [assetPackage.assets]
+  );
+
   return (
     <>
       {children}
-      <ul>
-        {assetPackage.assets.map((asset) => (
-          <li>
-            <Link to={`/${prefix}/${asset.slug}`}>{asset.name}</Link>
-          </li>
-        ))}
-      </ul>
+      {[{ slug: "", name: "", description: "" }, ...assetPackage.groups].map(
+        (group) => (
+          <>
+            {group.name && <h1>{group.name}</h1>}
+            {group.description && <p>{group.description}</p>}
+            {groupedAssets[group.slug] && (
+              <ul>
+                {groupedAssets[group.slug]?.map((asset) => (
+                  <li>
+                    <Link to={`/${prefix}/${asset.slug}`}>{asset.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </>
+        )
+      )}
     </>
   );
 }
