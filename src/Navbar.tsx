f049351f73ac7a561b2528e7d549526ee0f7ca17
Bill Garrett <garrett@astralfrontier.org>
2025-12-07 11:09:22 -0800

    Make navbar responsive

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,16 +1,31 @@
 import { NavLink } from "react-router";
 
+import styles from "./Navbar.module.scss";
+import { useCallback, useState } from "react";
+
 function Navbar() {
+  const [visible, setVisible] = useState<boolean>(true);
+
+  const toggleVisible = useCallback(
+    () => setVisible((isVisible) => !isVisible),
+    [setVisible]
+  );
+
   return (
     <>
       <header className="container">
-        <nav>
+        <nav className={styles.responsive}>
           <ul>
+            <li className={styles.burger} onClick={toggleVisible}>
+              &#8801;
+            </li>
             <li>
-              <strong>Breathtaking Comics</strong>
+              <NavLink to="/">
+                <strong>Breathtaking Comics</strong>
+              </NavLink>
             </li>
           </ul>
-          <ul>
+          <ul className={visible ? styles.visible : styles.invisible}>
             <li>
               <NavLink to="/rules/eic-edition">EIC Edition</NavLink>
             </li>
