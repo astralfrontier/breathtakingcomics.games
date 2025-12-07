2a0098b58e2bb39b6f73a040768165d8eb7a0acf
Bill Garrett <garrett@astralfrontier.org>
2025-12-07 09:24:10 -0800

    Add TOC to Traits

--- src/rules/SampleTraits.tsx
+++ src/rules/SampleTraits.tsx
@@ -1,13 +1,44 @@
 import { groupBy } from "ramda";
 import traitData from "../traits";
-import { useMemo } from "react";
+import type { TocItem } from "rehype-mdx-toc";
+import ArticleWithToc from "../ArticleWithToc";
+import slugify from "slugify";
 
-function SampleTraits() {
-  const groupedTraits = useMemo(
-    () => groupBy((trait) => trait.type, traitData),
-    []
+const slug = (value: string) => slugify(value, { lower: true });
+
+const groupedTraits = groupBy((trait) => trait.type, traitData);
+
+function buildTocFromTraits(): TocItem[] {
+  const elements: TocItem[] = [];
+
+  ["Position", "Past", "Personality", "Powers", "Problems"].forEach(
+    (traitType) => {
+      const slugged = slug(traitType);
+      elements.push({
+        depth: 1,
+        value: traitType,
+        numbering: [],
+        id: slugged,
+        href: `#${slugged}`,
+      });
+
+      groupedTraits[traitType.toLowerCase()]!.forEach((element) => {
+        const slugged = slug(element.name);
+        elements.push({
+          depth: 2,
+          value: element.name,
+          numbering: [],
+          id: slugged,
+          href: `#${slugged}`,
+        });
+      });
+    }
   );
 
+  return elements;
+}
+
+function SampleTraitsArticle() {
   return (
     <>
       <p>
@@ -17,10 +48,10 @@ function SampleTraits() {
       {["Position", "Past", "Personality", "Powers", "Problems"].map(
         (traitType) => (
           <>
-            <h1>{traitType} Traits</h1>
+            <h1 id={slug(traitType)}>{traitType} Traits</h1>
             {groupedTraits[traitType.toLowerCase()]!.map((trait) => (
               <>
-                <h3>{trait.name}</h3>
+                <h2 id={slug(trait.name)}>{trait.name}</h2>
                 <p>&quot;{trait.catchphrase}&quot;</p>
                 <p>Examples:</p>
                 <ul>
@@ -50,4 +81,8 @@ function SampleTraits() {
   );
 }
 
-export default SampleTraits;
+const toc = buildTocFromTraits();
+
+export default function SampleTraits() {
+  return <ArticleWithToc article={<SampleTraitsArticle />} toc={toc} />;
+}
