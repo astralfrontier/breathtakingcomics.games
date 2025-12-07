adff610c2008953e19ed1ad7be93754cca68e60d
Bill Garrett <garrett@astralfrontier.org>
2025-12-07 08:32:31 -0800

    Add MDX TOC plugins

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import mdx from "@mdx-js/rollup";
 import ViteYaml from "@modyfi/vite-plugin-yaml";
+import rehypeSlug from "rehype-slug";
+import rehypeMdxToc from "rehype-mdx-toc";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +12,7 @@ export default defineConfig({
     {
       enforce: "pre",
       ...mdx({
+        rehypePlugins: [rehypeSlug, rehypeMdxToc],
         /* jsxImportSource: …, otherOptions… */
       }),
     },
