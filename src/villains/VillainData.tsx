d19dcdc5b3d8be1f62572c647d4d7b530c8def7b
Bill Garrett <garrett@astralfrontier.org>
2025-12-08 10:42:32 -0800

    Villain: Handmaid

--- src/villains/VillainData.tsx
+++ src/villains/VillainData.tsx
@@ -1,10 +1,15 @@
 import type { BreathtakingAsset } from "../assets";
+import Handmaid, { frontmatter as handmaidMetadata } from "./handmaid.mdx";
 import OaklandOgre, {
   frontmatter as oaklandOgreMetadata,
 } from "./oakland-ogre.mdx";
 import Stitches, { frontmatter as stitchesMetadata } from "./stitches.mdx";
 
 const villainAssets: BreathtakingAsset[] = [
+  {
+    element: <Handmaid />,
+    metadata: handmaidMetadata,
+  },
   {
     element: <OaklandOgre />,
     metadata: oaklandOgreMetadata,
