593dbde455b7091c292d28db7293824d27713ec0
Bill Garrett <garrett@astralfrontier.org>
2025-12-19 07:50:09 -0800

    Add Nix flake files

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "The breathtakingcomics.games website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        packagename = "breathtakingcomics.games";
+        pkgs = nixpkgs.legacyPackages.${system};
+        builddeps = with pkgs; [
+          nodejs
+        ];
+      in
+      {
+        packages.default = pkgs.buildNpmPackage {
+          name = packagename;
+          buildInputs = builddeps;
+          src = self;
+          npmDeps = pkgs.importNpmLock {
+            npmRoot = ./.;
+          };
+          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+          installPhase = ''
+            mkdir $out
+            cp -r dist/. $out/
+          '';
+        };
+        devShell = pkgs.mkShell {
+          name = packagename;
+          packages = builddeps;
+        };
+      }
+    );
+}
