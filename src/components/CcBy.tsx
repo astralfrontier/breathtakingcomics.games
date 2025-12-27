00705397838b74984bba8f48d236ff07587bd6eb
Bill Garrett <garrett@astralfrontier.org>
2025-12-26 20:34:14 -0800

    Add MDX support

---
+++ src/components/CcBy.tsx
@@ -0,0 +1,19 @@
+export default function CcBy() {
+  return (
+    <article>
+      <a href="https://breathtakingcomics.games/">Breathtaking Comics</a> © 2025
+      by <a href="https://example.com">Bill Garrett</a> is licensed under{" "}
+      <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>{" "}
+      <img
+        src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"
+        alt=""
+        style={{ maxWidth: "1em;max-height:1em;margin-left: .2em;" }}
+      />
+      <img
+        src="https://mirrors.creativecommons.org/presskit/icons/by.svg"
+        alt=""
+        style={{ maxWidth: "1em;max-height:1em;margin-left: .2em;" }}
+      />
+    </article>
+  );
+}
