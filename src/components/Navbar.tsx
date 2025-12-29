b48e5da72dca7325d1ebfd2f53fe46a3d5a3d255
Bill Garrett <garrett@astralfrontier.org>
2025-12-29 12:59:10 -0800

    Add the Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,52 @@
+import styles from "./Navbar.module.scss";
+import { useCallback, useState } from "react";
+
+function Navbar() {
+  const [visible, setVisible] = useState<boolean>(false);
+
+  const toggleVisible = useCallback(
+    () => setVisible((isVisible) => !isVisible),
+    [setVisible]
+  );
+
+  return (
+    <>
+      <header className="container">
+        <nav className={styles.responsive}>
+          <ul>
+            <li className={styles.burger}>
+              <button onClick={toggleVisible}>&#8801;</button>
+            </li>
+            <li>
+              <a href="/">
+                <strong>Breathtaking Comics</strong>
+              </a>
+            </li>
+          </ul>
+          <ul className={visible ? styles.visible : styles.invisible}>
+            <li>
+              <a href="/rules/eic-edition">EIC Edition</a>
+            </li>
+            <li>
+              <a href="/rules/melvin-edition">Melvin Edition</a>
+            </li>
+            <li>
+              <a href="/rules/traits">Traits</a>
+            </li>
+            <li>
+              <a href="/heroes">Heroes</a>
+            </li>
+            <li>
+              <a href="/villains">Villains</a>
+            </li>
+            <li>
+              <a href="/support">Supporting Cast</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
