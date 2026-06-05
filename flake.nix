0da241649fcb204d07a03b51675dd265c45e8386
Bill Garrett <garrett@astralfrontier.org>
2026-06-05 09:57:02 -0700

    Update Node version

--- flake.nix
+++ flake.nix
@@ -10,23 +10,26 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         packagename = "breathtakingcomics.games";
+        packageversion = "1.0.0";
         pkgs = nixpkgs.legacyPackages.${system};
         builddeps = with pkgs; [
-          nodejs_22
+          nodejs
         ];
       in
       {
         packages.default = pkgs.buildNpmPackage {
-          name = packagename;
+          pname = packagename;
+          version = packageversion;
           buildInputs = builddeps;
-          src = self;
-          npmDeps = pkgs.importNpmLock {
-            npmRoot = ./.;
-          };
-          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+          src = ./.;
+          # npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          npmDepsHash = "sha256-xP4dXB7U8acQBSCNwvEhg9e6vukuKe5kL7MRS5Xmio0=";
+          npmFlags = [ "--legacy-peer-deps" ];
+          makeCacheWritable = true;
+          # https://github.com/sass/embedded-host-node/issues/334
           installPhase = ''
             mkdir $out
-            cp -r out/. $out/
+            cp -r dist/. $out/
           '';
         };
         devShell = pkgs.mkShell {
